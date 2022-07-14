2ca433e349e96d11a74d128be1959df6da0a0ecc
alkaff <alkaff@prixa.ai>
2022-07-14 11:43:58 +0700

    replace graphql_flutter with graphql

---
+++ test/country_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pretest_kitalulus_2/providers/main_provider.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  //
+  group("country test", () {
+    test("indonesia should be exist", () {});
+  });
+}
